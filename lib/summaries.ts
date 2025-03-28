91fd6b18d06f9499db1e766230818f024424a206
Fudail Mohammed Zafar <142711828+fudailzafar@users.noreply.github.com>
2025-03-28 17:47:47 +0530

    Individual Summary Page

--- lib/summaries.ts
+++ lib/summaries.ts
@@ -6,3 +6,27 @@ export async function getSummaries(userId: string) {
     await sql`SELECT * FROM pdf_summaries WHERE user_id = ${userId} ORDER BY created_at DESC`;
   return summaries;
 }
+
+export async function getSummaryById(id: string) {
+  try {
+    const sql = await getDbConnection();
+
+    const [summary] = await sql`SELECT 
+    id,
+    user_id,
+    title, 
+    original_file_url, 
+    summary_text,  
+    created_at, 
+    updated_at, 
+    status, 
+    file_name,
+    LENGTH(summary_text) - LENGTH(REPLACE(summary_text, ' ', '')) + 1 AS word_count 
+    FROM pdf_summaries WHERE id = ${id}`;
+
+    return summary;
+  } catch (err) {
+    console.error("Error fetching summary by id", err);
+    return null;
+  }
+}
