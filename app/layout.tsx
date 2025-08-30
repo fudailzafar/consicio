ee7906d9085c1d775a0440d6376089edb5871bae
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-08-30 19:59:46 +0530

    feat(analytics): vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/next";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -70,7 +71,10 @@ export default function RootLayout({
         <body className={`${fontSans.variable} font-sans antialiased`}>
           <div className="relative flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1">{children}</main>
+            <main className="flex-1">
+              {children}
+              <Analytics />
+            </main>
             <Footer />
           </div>
           <Toaster />
